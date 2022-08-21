32077cca36d0b2bb39e5587b94f7a8102ffbc7df
raanw <919940399@qq.com>
2022-08-21 12:44:09 +0800

    标注模块 添加列表 图标 等新增功能 修改部分标注bug 修复因e.pagex导致的坐标获取问题

--- src/config/config.js
+++ src/config/config.js
@@ -1,13 +1,13 @@
 ﻿global.AppConfig = {
-    tiffPicsIP:'http://192.168.3.5:8085//XrayImageDB//',
+    tiffPicsIP:'http://172.24.71.145:8080//XrayImageDB//',
 
     //serverIP为java后端服务器地址
-    serverIP:'http://172.23.101.71:8085',
+    serverIP:'http://172.24.71.145:8085',
 
     //aiIP为ai检测的服务器地址
-    //aiIP:'http://192.168.3.5:8888',
+    aiIP:'http://172.24.71.145:8888',
 
     //XrayDB
-    XrayDBIP:'http://172.23.101.71:8085//XrayImageDB//'
+    XrayDBIP:'http://172.24.71.145:8080//XrayImageDB//'
 
 };
